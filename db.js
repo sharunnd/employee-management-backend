cda06d074f980e9b91f8b9a064cdffe11de81642
Your Name <sharunnd11@gmail.com>
2023-07-13 14:08:14 +0530

    done

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async() =>{
     try {
-        const connection = mongoose.connect(process.env.MONGO_URL)
+        const connection =await mongoose.connect(process.env.MONGO_URL)
         console.log("connected to DB");
     } catch (error) {
         console.log(error.message);
